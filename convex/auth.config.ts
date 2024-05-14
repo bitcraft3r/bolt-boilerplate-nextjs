ed38b6dddfa04bf81445f10d09266f148a6f89ca
FleetAdmiralJakob <jakob.roessner@outlook.de>
2024-05-14 15:04:37 +0200

    refactor: switch to pnpm
    - Cleaned up docs and auth.config.ts
    
    - Switched to pnpm

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 export default {
     providers: [
         {
-            domain: "https://polished-pangolin-38.clerk.accounts.dev/",
+            domain: process.env.CLERK_JWT_ISSUER_DOMAIN,
             applicationID: "convex",
         },
     ]
