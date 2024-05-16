6084d8c494984cdbd94c99551368866e392df0bb
SOV3333 <8282076+bitcraft3r@users.noreply.github.com>
2024-05-16 22:22:58 +1200

    style: fix width of posts section

--- app/(crud)/app.tsx
+++ app/(crud)/app.tsx
@@ -3,9 +3,11 @@ import { AllPosts } from "./all-posts"
 
 export const App = () => {
     return (
-        <div>
-            <CreatePost />
-            <AllPosts />
+        <div className="w-full flex">
+            <div className="flex-1">
+                <CreatePost />
+                <AllPosts />
+            </div>
         </div>
     )
 }
--- app/_home/hero.tsx
+++ app/_home/hero.tsx
@@ -4,8 +4,10 @@ import { AllPosts } from "../(crud)/all-posts"
 export const Hero = () => {
     return (
         <div className="flex flex-col items-center justify-center max-w-3xl">
-            <CreatePost />
-            <AllPosts />
+            <div className="flex-1">
+                <CreatePost />
+                <AllPosts />
+            </div>
         </div>
     )
 }
